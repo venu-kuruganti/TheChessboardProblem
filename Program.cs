79f5acbb75fd986c7e39d77fdf59bcbc9aae969a
Venu Kuruganti <35195194+venu-kuruganti@users.noreply.github.com>
2022-06-24 20:52:12 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace TheChessboardProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting the chessboard problem...");
+
+            int i = 1;
+            double j = 1;
+
+            while (i <= 64)
+            {
+
+                Console.Write("{0}\t", j.ToString());
+
+                j *= 2;
+
+                if (i%8==0)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine();
+                }
+
+                i++;
+            }//End of while
+
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
